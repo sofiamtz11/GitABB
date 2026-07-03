37d3f0a298415979ac163e549d35ced2d9c12bbc
Renata Martinez <25430048@uttcampus.edu.mx>
2026-07-03 13:16:05 -0600

    Ya hice un cambio en la línea 6 CAMBIALA TU TMB

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
+
 public class Main {
         public static void main(String[] args) {
                 System.out.println("HELLO WORLD CUP 2026");
+                System.out.println("HAGAMOS UN CONFLICTO");
         }
 }
