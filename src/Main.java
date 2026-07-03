5df3b8defcfaab3191cae843bc226b1f38f7022d
Renata Martinez <25430048@uttcampus.edu.mx>
2026-07-03 13:30:19 -0600

    MUNDIAL 2026 MEXICO

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,15 @@ public class Main {
         public static void main(String[] args) {
                 System.out.println("HELLO WORLD CUP 2026");
                 System.out.println("VIVA MÉXICOOOOOOOOO0");
+                System.out.println(" ____________________________________________ ");
+                System.out.println("|                 |        |                 |");
+                System.out.println("|======|          |        |          |======|");
+                System.out.println("|      |          |   __   |          |      |");
+                System.out.println("|---|  |          | /    \\ |          |  |---|");
+                System.out.println("|   |  |          | |  ⚽  |          |  |   |");
+                System.out.println("|---|  |          | \\ __ / |          |  |---|");
+                System.out.println("|      |          |        |          |      |");
+                System.out.println("|======|          |        |          |======|");
+                System.out.println("|_________________|________|_________________|");
         }
 }
